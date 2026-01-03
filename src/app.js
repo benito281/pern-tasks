705f2002884daa139ae0da71123c6f19aceea421
benito281 <benitoortiz281@gmail.com>
2026-01-03 00:05:23 -0300

    Rutas de autenticacion

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import morgan from 'morgan';
 import taskRoutes from './routes/tasks.routes.js'
+import authRoutes from './routes/auth.routes.js'
 const app = express();
 
 app.use(morgan('dev'))
