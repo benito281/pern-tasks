c2297576efce4824b19d5087c95f746c61c7198c
benito281 <benitoortiz281@gmail.com>
2026-01-03 16:42:54 -0300

    Controller añadidos

---
+++ src/controllers/tasks.controller.js
@@ -0,0 +1,10 @@
+//TODO: Copiar funciones que se encuentran en tasks.routes
+export const getAllTasks = (req,res) => res.send('Obteniendo tareas');
+
+export const getTask =  (req,res) => res.send('Obteniendo tarea unica');
+
+export const createTask = (req,res) => res.send('Creando tarea');
+
+export const updateTask =  (req,res) => res.send('Actualizando tarea');
+
+export const deleteTask = (req,res) => res.send('Eliminando tarea');
