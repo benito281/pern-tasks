e50cdb83de7dc786304152289075c2503871f244
benito281 <benitoortiz281@gmail.com>
2026-01-02 23:51:56 -0300

    Rutas de tareas

--- src/app.js
+++ src/app.js
@@ -1,13 +1,17 @@
 import express from 'express';
 import morgan from 'morgan';
-
-
-
+import taskRoutes from './routes/tasks.routes.js'
 const app = express();
 
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({extended: false}));
+
+
+
+app.use(taskRoutes)
+
+
 app.get("/", (req, res) => {
     res.json({
         message:"Welcome to my API"
