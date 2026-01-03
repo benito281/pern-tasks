b36e2c89604c16674694eb25bff0be06f94dac26
benito281 <benitoortiz281@gmail.com>
2026-01-03 00:07:25 -0300

    Rutas de autenticacion y tareas agregadas

--- src/app.js
+++ src/app.js
@@ -4,27 +4,27 @@ import taskRoutes from './routes/tasks.routes.js'
 import authRoutes from './routes/auth.routes.js'
 const app = express();
 
+//Middlewares
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({extended: false}));
 
-
-
-app.use(taskRoutes)
-
-
+//Rutas
 app.get("/", (req, res) => {
     res.json({
         message:"Welcome to my API"
     })
 })
+app.use(taskRoutes)
+app.use(authRoutes)
+
 
 app.get('/test', (req, res) => {
     throw new Error('My custom error!')
     res.send("test")
 })
 
-
+//Error handler
 app.use((err, req, res, next) => {
     res.status(500).json({
         status: "Error",
